6b758165a751ee7437ae71f4d5ba920b561bb332
jmpurtle <purtlejohnmark@gmail.com>
2022-07-19 18:33:45 -0500

    Integrate portfolio

---
+++ app/appEnv.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'debug' => false,
+    'dbConfig' => array(
+    	'dbHost' => 'localhost',
+		'dbPort' => '3306',
+		'dbName' => 'leads',
+		'dbUser' => 'dbuser',
+		'dbPass' => 'dbpass'
+    )
+);
